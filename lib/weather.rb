06581c061269760c67b6bb493e19224ed6d68edf
davdkm <davd.km@gmail.com>
2016-01-30 22:57:34 -0800

    add temp forecast for next hour

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,20 +1,17 @@
 require 'pry'
-require 'net/http'
-require 'json'
+require 'open-uri'
+require 'nokogiri'
 
 class Weather
   @@city = ''
   @@country = ''
 
+  attr_accessor :temp, :humidity, :location, :temp, :humidity, :cloud, :precipitation, :symbol, :wind, :time
+
   def self.get_input
+    puts "Please enter the name of the city or zip code, country"
     input = gets.strip
     @@city, @@country = input.split(", ").first, input.split(", ").last
-  end
-
-  def self.api_call
-    http = "http://api.openweathermap.org/data/2.5/forecast?q=#{self.city},#{self.country}&mode=xml&APPID=a44943d6e75bdea08371d2e9fb59e9a2"
-    response = Net::HTTP.get(URI(http))
-    puts response
   end
 
   def self.city
@@ -23,6 +20,18 @@ class Weather
 
   def self.country
     @@country
+  end
+
+  def self.call
+    # Gets weather data
+    xml = Nokogiri::HTML(open("http://api.openweathermap.org/data/2.5/forecast?q=#{self.city},#{self.country}&mode=xml&units=imperial&APPID=a44943d6e75bdea08371d2e9fb59e9a2"))
+    binding.pry
+    weather = {}
+    xml.css("weatherdata").each do |element|
+      weather[:location] = element.css("location name").text
+      weather[:time] = element.css("forecast time").attribute("from").value
+      weather[:temp] = element.css("forecast time temperature").attribute("value").value
+    end
   end
 
 end
