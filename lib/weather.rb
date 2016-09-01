d11d0ba9567d4139c23cfcf007da4b2b0e003250
davdkm <davd.km@gmail.com>
2016-08-31 20:14:38 -0700

    changed date and times to reader friendly formats

--- lib/weather.rb
+++ lib/weather.rb
@@ -21,13 +21,13 @@ class Weather
   end
 
   def display_forecast(day)
-    puts "Here's the weather forecast for " + "#{self.location}".colorize(:red) + " on " + "#{day}.".colorize(:light_blue)
+    puts "Here's the weather forecast for " + "#{self.location}".colorize(:red) + " on " + "#{DateTime.parse(day).strftime('%A - %b %-d, %Y')}.".colorize(:light_blue)
 
     chosen_date = find_day(day)
     @forecast.each do |item|
       if item[:time].split("T").first == day
         puts "-------------------"
-        puts "At " + "#{item[:time].split("T").last}:".colorize(:red) + " #{item[:condition]}.".colorize(:light_blue)
+        puts "At" + "#{DateTime.parse(item[:time]).strftime('%l %P')}:".colorize(:red) + " #{item[:condition]}.".colorize(:light_blue)
         puts "The temperature will be " + "#{item[:temp]}F.".colorize(:red)
         puts "Humidity will be at " + "#{item[:humidity]}%.".colorize(:light_blue)
       end
@@ -36,7 +36,7 @@ class Weather
 
   def day_to_forecast
     days = self.split_time
-    days.each_with_index{|date, index| puts "#{index + 1}. #{date}"}
+    days.each_with_index{|date, index| puts "#{index + 1}. #{Date.parse(date).strftime('%A - %b %-d, %Y')}"}
   end
 
   def split_time
